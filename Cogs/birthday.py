b1753b2995e7656dea3f799fc80133fad20a06a3
Brian Lin <pyrhex@gmail.com>
2023-05-18 20:08:46 -0700

    Updated with skeleton code

--- Cogs/birthday.py
+++ Cogs/birthday.py
@@ -1,12 +1,12 @@
 import datetime
+import os
+import json
+from brainbot import guildList
 from discord.ext import commands, tasks
-
-utc = datetime.timezone.utc
-
-# If no tzinfo is given then UTC is assumed.
-times = [
-    datetime.time(hour=8, tzinfo=utc),
-]
+from discord.ext import commands
+from discord.commands import slash_command
+def getBirthdaysToday():
+    pass
 
 class BirthdayReminders(commands.Cog):
     def __init__(self, bot):
@@ -15,7 +15,30 @@ class BirthdayReminders(commands.Cog):
 
     def cog_unload(self):
         self.reminders.cancel()
-
-    @tasks.loop(time=times)
+    # @slash_command(name='birthday', description="This will fetch the specified person's birthday", guild_ids=guildList)
+    # async def birthday(self, ctx):
+    #     today = date.today()
+    #     json_file_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'birthdays.json'))
+    #     with open(json_file_path, 'r+') as file:
+    #     # First we load existing data into a dict.
+    #         file_data = json.load(file)
+    #         for i in file_data["birthdays"]:
+    #             birthday = date(1999p, i["month"], i["day"])
+    #             if(birthday.month == today.month and birthday.day == today.day):
+    #                 await ctx.respond("It's " + i["name"] + "'s birthday today!")
+    #                 return
+    @tasks.loop(time=datetime.time(hour=16, minute=0, second=0, tzinfo=datetime.timezone.utc))
     async def reminders(self):
-        pass
+        # today = date.today()
+        # json_file_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'birthdays.json'))
+        # with open(json_file_path, 'r+') as file:
+        # # First we load existing data into a dict.
+        #     file_data = json.load(file)
+        #     for i in file_data["birthdays"]:
+        #         birthday = date(1999, i["month"], i["day"])
+        #         if(birthday.month == today.month and birthday.day == today.day):
+        #             print("It's " + i["name"] + "'s birthday today!")
+        #             return
+        print("hi")
+def setup(bot):
+    bot.add_cog(BirthdayReminders(bot))
